678f09cb1a435bd40675931e812baa7df5810512
flori-s <150121780+flori-s@users.noreply.github.com>
2024-05-13 10:19:27 +0200

    The login function is retreiving passwords and emails. It also hases the password.

---
+++ app/src/main/java/com/floris/mediarchive/Extensions.kt
@@ -0,0 +1,77 @@
+package com.floris.mediarchive
+
+import java.security.MessageDigest
+import java.util.regex.Pattern
+
+/**
+ * Converts a string to a SHA-256 hash.
+ *
+ * This function first converts the string to a byte array. Then, it gets an instance of the SHA-256 MessageDigest,
+ * and uses it to digest the byte array. Finally, it joins the resulting byte array into a string, with each byte
+ * represented as a two-digit hexadecimal number.
+ *
+ * @return The SHA-256 hash of the string.
+ */
+fun String.toSHA256(): String {
+    val bytes = this.toByteArray()
+    val md = MessageDigest.getInstance("SHA-256")
+    val digest = md.digest(bytes)
+    return digest.joinToString("") { "%02x".format(it) }
+}
+
+/**
+ * Capitalizes the first letter of each word in a string.
+ *
+ * This function splits the string into words (separated by spaces), capitalizes the first letter of each word,
+ * and then joins the words back together with spaces in between.
+ *
+ * @return The string with the first letter of each word capitalized.
+ */
+fun String.capitalizeWords(): String = split(" ").joinToString(" ") { it.capitalize() }
+
+/**
+ * Checks if a string is a valid email address.
+ *
+ * This function uses a regular expression to check if the string matches the general structure of an email address.
+ * The regular expression used checks for:
+ * - Between 1 and 256 alphanumeric characters, including +, ., _, %, -, and +, before the @ symbol.
+ * - An @ symbol.
+ * - Between 1 and 64 alphanumeric characters, including -, after the @ symbol and before a period.
+ * - A period followed by between 1 and 25 alphanumeric characters, including -, after the @ symbol.
+ * - This pattern can be repeated one or more times.
+ *
+ * @return Boolean value indicating whether the string is a valid email address.
+ */
+fun String.isValidEmail(): Boolean {
+    val emailPattern = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" + // Between 1 and 256 alphanumeric characters, including +, ., _, %, -, and +, before the @ symbol.
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + // Between 1 and 64 alphanumeric characters, including -, after the @ symbol and before a period.
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + // A period followed by between 1 and 25 alphanumeric characters, including -, after the @ symbol.
+                ")+" // This pattern can be repeated one or more times.
+    )
+    return emailPattern.matcher(this).matches()
+}
+
+/**
+ * Checks if a string is a valid password.
+ *
+ * This function uses a regular expression to check if the string meets the following password requirements:
+ * - At least 8 characters long.
+ * - Contains at least one digit.
+ * - Contains at least one letter.
+ * - Contains at least one special character (@, #, $, %, ^, &, +, =).
+ *
+ * @return Boolean value indicating whether the string is a valid password.
+ */
+fun String.isValidPassword(): Boolean {
+    val passwordPattern = Pattern.compile(
+        "^(?=.*[0-9])" +         //at least 1 digit
+                "(?=.*[a-zA-Z])" +  //at least 1 letter
+                "(?=.*[@#$%^&+=!])" +  //at least 1 special character
+                ".{8,}"               //at least 8 characters
+    )
+    return passwordPattern.matcher(this).matches()
+}
--- app/src/main/java/com/floris/mediarchive/LoginActivity.kt
+++ app/src/main/java/com/floris/mediarchive/LoginActivity.kt
@@ -1,15 +1,65 @@
 package com.floris.mediarchive
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
 
+    fun login(view: View) {
+        // Get the email and password fields
+        val emailEdittext = findViewById<EditText>(R.id.editTextTextEmailAddress_LoginActivity_xml)
+        val passwordEditText = findViewById<EditText>(R.id.editTextTextPassword_LoginActivity_xml)
+
+        // Check if the email and password fields are not empty
+        if (emailEdittext.text.toString().isNotEmpty() && passwordEditText.text.toString()
+                .isNotEmpty()
+        ) {
+            // Check if the email and password fields are valid
+            if (emailEdittext.text.toString().isValidEmail() && passwordEditText.text.toString()
+                    .isValidPassword()
+            ) {
+                val email = emailEdittext.text.toString()
+                val hasedPassword = passwordEditText.text.toString().toSHA256()
+
+                println("Email: $email")
+                println("Password: $hasedPassword")
+
+            }else
+            {
+                when {
+                    !emailEdittext.text.toString().isValidEmail() -> emailEdittext.error = "Invalid email!"
+                    !passwordEditText.text.toString().isValidPassword() -> passwordEditText.error = "Invalid password!"
+                }
+            }
+        } else {
+            when {
+                emailEdittext.text.toString().isEmpty() -> emailEdittext.error = "Email is required!"
+                passwordEditText.text.toString().isEmpty() -> passwordEditText.error = "Password is required!"
+            }
+        }
+    }
+
+    fun showPasswordRequirements(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Wachtwoord vereisten")
+        builder.setMessage(
+            "Wachtwoord moet minimaal: \n" +
+                    "- 8 karakters lang zijn. \n" +
+                    "- 1 letter bevatten. \n" +
+                    "- 1 cijfer bevatten. \n" +
+                    "- 1 speciaal karakter bevatten. (@#\$%^&+=!)"
+        )
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
     }
 }
